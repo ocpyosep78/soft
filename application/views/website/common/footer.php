2eb6b30e40e22f3db8b2c0ec4d283967101cee8c
her0satr <her0satr@yahoo.com>
2013-06-21 06:35:12 -0700

    Update

---
+++ application/views/website/common/footer.php
@@ -0,0 +1,20 @@
+<footer>
+	<div class="container-fluid">
+		<div class="row-fluid footer_section_pre">
+			
+			<div class="span12">
+				<p class="copy center"><a href="typography.html">Terms</a> | <a href="typography.html">Privacy</a> | <a href="typography.html">Typography</a> | <a href="typography.html">About</a></p>
+				<p class="copy center">All content &copy; 2013 <a href="http://appsarea.com/">AppsArea</a></p>
+				<br />
+			</div>
+		</div>
+    	
+	</div>
+</footer>
+
+<script type="text/javascript" src="<?php echo base_url('static/theme/job_board/js/jquery.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('static/theme/job_board/js/jquery-ui.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('static/theme/job_board/js/jquery.flot.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('static/theme/job_board/bootstrap/js/bootstrap.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('static/theme/job_board/js/global.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('static/theme/job_board/js/stylesheetToggle.js'); ?>"></script>
---
+++ application/views/website/common/meta.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta charset="utf-8">
+	<title>Job Board - Bootstrap</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="stylesheet" href="<?php echo base_url('static/theme/job_board/css/theme_switcher.css'); ?>" type="text/css">
+	<link rel="stylesheet" href="<?php echo base_url('static/theme/job_board/css/job_blue.responsive.css'); ?>" title="job_blue" type="text/css">
+	
+    <!--[if lt IE 9]>
+		<link rel="stylesheet" href="<?php echo base_url('static/theme/job_board/css/bootstrap_ie7.css'); ?>" title="job_blue" type="text/css">
+		<link rel="stylesheet" href="<?php echo base_url('static/theme/job_board/css/ie7.css'); ?>" title="job_blue" type="text/css">
+		<link rel="stylesheet" href="<?php echo base_url('static/theme/job_board/js/html5.js'); ?>" title="job_blue" type="text/css">
+	<![endif]-->
+</head>
