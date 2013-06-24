8b9fd7c76d768a474296b7dd70c0ec865eafc3fd
her0satr <her0satr@yahoo.com>
2013-06-23 17:08:48 -0700

    More Page

---
+++ application/views/website/common/info.php
@@ -0,0 +1,23 @@
+<h3>Posting a job is Free!!!</h3>
+
+<div class="row-fluid form-tooltip">	
+	
+	<div class="span12">
+		<h4>Reach thousands of users</h4>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. In congue turpis sed enim posuere malesuada. Aliquam a urna et dolor blandit tincidunt.
+	</div>	
+</div>	
+<div class="row-fluid">
+	
+	<div class="span12">
+		<h4>View CVs instantly</h4>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. In congue turpis sed enim posuere malesuada. Aliquam a urna et dolor blandit tincidunt.
+	</div>			
+</div>			
+<div class="row-fluid">
+	
+	<div class="span12">
+		<h4>Integrated analytics</h4>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. In congue turpis sed enim posuere malesuada. Aliquam a urna et dolor blandit tincidunt.
+	</div>
+</div>
